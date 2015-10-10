4a90083475c23fe06ba4611acc1934f704a9f9e7
Keven Jesus <keven.jesus@grupokrs.com.br>
2015-10-10 18:24:27 -0300

    change of link in the pictures of the participants

--- docpad.js
+++ docpad.js
@@ -8,7 +8,7 @@ module.exports = {
     // Conference info
     conf: {
       name: "Web Design Boteconferencis",
-      description: "O Web Design Boteconferencis vem com uma ideia inovadora no cenário de tecnologia, onde criamos um espaço propício para elevar seu networking para outro nível. Temos espaço para todos, seja você designer, programador ou empreendedor!",
+      description: "Conference description",
       date: "26 de Novembro às 20hrs",
       // If your event is free, just comment this line
       price: "R$30,00",
@@ -65,7 +65,7 @@ module.exports = {
     schedule: [
       {
         name: "Gabriel Benarrós",
-        photo: "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-frc3/v/t1.0-9/548900_10151326446686436_1006774967_n.jpg?oh=906016226a80aa8a72b9b1e29f4ca879&oe=5684C697&__gda__=1456055770_16d509b04935bbc47ae1b5c577b65e19",
+        photo: "http://i.imgur.com/JsZ5ER9.jpg",
         bio: "Empreendedor fundador da Ingresse.",
         company: "Ingresse",
         link: {
@@ -75,8 +75,8 @@ module.exports = {
       },
       {
         name: "David Ruiz",
-        photo: "https://scontent-atl3-1.xx.fbcdn.net/hphotos-xlp1/v/t1.0-9/12088294_10153269676857956_8327263627422645534_n.jpg?oh=427c0751f3c3c25731271c536b76e4b3&oe=56CBD53F",
-        bio: "CTO da Ingresse e da Futura Networks (Campus Party), representante da Mozilla",
+        photo: "http://i.imgur.com/BUZmzZA.jpg",
+        bio: "CTO da Ingresse e da Campus Party, representante da Mozilla",
         company: "Ingresse, Campus Party e Mozilla",
         link: {
           href: "https://br.linkedin.com/in/wupsbr",
@@ -85,7 +85,7 @@ module.exports = {
       },
       {
         name: "Daniel Borlino",
-        photo: "https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xtf1/v/t1.0-9/10583981_767334813303798_7989639994818683624_n.jpg?oh=eef76e93cc22e543f7d7967cab008134&oe=56CDC85E&__gda__=1456648453_5652966b699c76e203619028d9edf975",
+        photo: "http://i.imgur.com/meaa2sc.jpg",
         bio: "Organizador do Boteconference, Sócio no Meatless e Engenheiro Fron-end especializado em Angular.js na Ingresse.com.",
         company: "Ingresse e Meatless",
         link: {
@@ -95,7 +95,7 @@ module.exports = {
       },
       {
         name: "Filipe Santos",
-        photo: "https://scontent-atl3-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/11822295_999105336809024_9126701963551687503_n.jpg?oh=c96ce20d8ea60722cf58095044c747a3&oe=56915849",
+        photo: "http://i.imgur.com/PWJYfle.jpg",
         bio: "Organizador do Boteconference, Fundador da EX3 Comunicação, programador fron-end.",
         company: "EX3 Comunicação",
         link: {
@@ -105,8 +105,8 @@ module.exports = {
       },
       {
         name: "Keven Jesus",
-        photo: "https://scontent-atl3-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/12003945_714135088691588_5403367596511610957_n.jpg?oh=37b2895f76023d09a1063da5e9099005&oe=56C978AA",
-        bio: "Organizador do Boteconference, programador fron-end.",
+        photo: "http://i.imgur.com/JGqLfHH.jpg",
+        bio: "Organizador do Boteconference, programador fron-end especializado em ReactJS.",
         company: "Grupo KRS",
         link: {
           href: "https://www.facebook.com/kevenjesus",
@@ -115,7 +115,7 @@ module.exports = {
       },
       {
         name: "Tereza Alux",
-        photo: "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xaf1/v/t1.0-1/p160x160/12032238_900831266674127_7902646899766538149_n.jpg?oh=991e291681e32cb2fa06aa5ddab0b2a6&oe=56D2FD8C&__gda__=1453259312_94f9cc5f161748b2e6980ca993f9d686",
+        photo: "http://i.imgur.com/heNiV9A.jpg",
         bio: "Organizadora do Boteconference, colunista no Choco la Design, Designer.",
         company: "Choco la Design",
         link: {
